96ad77386f014a6a1896ea73e5e754e0966ff730
EdgarSantiago <edgargsp777@gmail.com>
2023-05-04 16:19:02 -0300

    google ads

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,6 +13,15 @@
 <svelte:head>
 	<title>The Future of Wealth: How to make money with ChatGPT</title>
 <meta name="description" content="Discover the secrets to making money with ChatGPT! Our eBook, The Future of Wealth, reveals everything you need to know about this cutting-edge technology." />
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-309362205"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-309362205');
+</script>
 </svelte:head>
 
 <section>
